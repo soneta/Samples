20580d8bb4777997de637c29fd30e5a6d5e39d09
Tomasz Ściepłek <tomasz.scieplek@enova.pl>
2019-05-06 15:46:49 +0200

    Inicjalizacja solucji i podstawowych projektów

---
+++ src/Samples/ZmianaNazwTowarowWorker.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using Samples;
+using Soneta.Business;
+using Soneta.Tools;
+using Soneta.Towary;
+using Soneta.Types;
+
+[assembly: Worker(typeof(ZmianaNazwTowarowWorker), typeof(Towary))]
+
+namespace Samples
+{
+    /// <summary>
+    /// Wokrer oparty na przykładzie pochodzącym z repozytorium Soneta.Examples - https://github.com/soneta/Examples (Example 5)
+    /// </summary>
+    public class ZmianaNazwTowarowWorker : ContextBase
+    {
+        public ZmianaNazwTowarowWorker(Context context) : base(context)
+        {
+        }
+
+        // Potrzebne dla akcji parametry
+        [Context]
+        public ZmianaNazwTowarowParams Params
+        {
+            get;
+            set;
+        }
+
+        // Potrzebne dane na których zostanie wykonana akcja
+        [Context]
+        public Towar[] Towary
+        {
+            get; set;
+        }
+
+        // Akcja jaka zostanie wykonana na danych w oparciu o ustawione parametry
+        [Action("Soneta Examples/Zmiana postfiksu", Mode = ActionMode.SingleSession | ActionMode.ConfirmSave | ActionMode.Progress)]
+        public void ZmianaNazw()
+        {
+            using (var t = Params.Session.Logout(true))
+            {
+                foreach (var towar in Towary.Where(towar => Params.TypTowaru == towar.Typ))
+                {
+                    if (!Params.DodajPostfiks.IsNullOrEmpty() && !towar.Nazwa.StartsWith(Params.DodajPostfiks))
+                    {
+                        towar.Nazwa = towar.Nazwa + Params.DodajPostfiks;
+                    }
+
+                    if (!Params.UsunPostfiks.IsNullOrEmpty() && towar.Nazwa.EndsWith(Params.UsunPostfiks))
+                    {
+                        towar.Nazwa = towar.Nazwa.Substring(0, towar.Nazwa.Length - Params.UsunPostfiks.Length);
+                    }
+                }
+                t.Commit();
+            }
+        }
+    }
+
+    public class ZmianaNazwTowarowParams : ContextBase
+    {
+        public ZmianaNazwTowarowParams(Context context) : base(context)
+        {
+            TypTowaru = TypTowaru.Towar;
+        }
+
+        public TypTowaru TypTowaru { get; set; }
+
+        public string DodajPostfiks { get; set; }
+
+        [Caption("Usuń postfiks")]
+        public string UsunPostfiks { get; set; }
+    }
+}
---
+++ tests/Samples.Tests/ZmianaNazwTowarowWorkerTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using NUnit.Framework;
+using Soneta.Test;
+using Soneta.Towary;
+
+namespace Samples.Tests
+{
+    [TestFixture]
+    public class ZmianaNazwTowarowWorkerTests : DbTransactionTestBase
+    {
+        [Test]
+        public void ZmianaNazw_AddPostfix_PostfixIsAdded()
+        {
+            // Arrange
+            var worker = CreateWorker(new[] { Bikini }, TypTowaru.Towar, "xxx");
+
+            // Act
+            worker.ZmianaNazw();
+
+            // Assert
+            Assert.AreEqual("Bikini - Strój kąpielowy damskixxx", Bikini.Nazwa);
+        }
+
+        [Test]
+        public void ZmianaNazw_AddPostfixToSpecifiedTypeOnly_PostfixIsAddedToSpecifiedTypleOnly()
+        {
+            // Arrange
+            var worker = CreateWorker(new[] { Bikini, Montaz }, TypTowaru.Usługa, "xxx");
+
+            // Act
+            worker.ZmianaNazw();
+
+            // Assert
+            Assert.AreEqual("Bikini - Strój kąpielowy damski", Bikini.Nazwa);
+            Assert.AreEqual("Montaż wiązań narciarskichxxx", Montaz.Nazwa);
+        }
+
+        [Test]
+        public void ZmianaNazw_AddPostfixToMultipleArticles_PostfixIsAddedToMultipleArticles()
+        {
+            // Arrange
+            var worker = CreateWorker(new[] { Bikini, Buty45 }, TypTowaru.Towar, "xxx");
+
+            // Act
+            worker.ZmianaNazw();
+
+            // Assert
+            Assert.AreEqual("Bikini - Strój kąpielowy damskixxx", Bikini.Nazwa);
+            Assert.AreEqual("Buty do nart Extreme 45xxx", Buty45.Nazwa);
+        }
+
+        // Houston, we have a problem
+        // There is a bug in existing sample code
+        //[Test]
+        //public void ZmianaNazw_AddPostfixWhenPostfixAlreadyExists_PostfixShouldNotBeAdded()
+        //{
+        //    // Arrange
+        //    var worker = CreateWorker(new[] { Bikini }, TypTowaru.Towar, "damski");
+
+        //    // Act
+        //    worker.ZmianaNazw();
+
+        //    // Assert
+        //    Assert.AreEqual("Bikini - Strój kąpielowy damski", Bikini.Nazwa);
+        //}
+
+        [Test]
+        public void ZmianaNazw_RemoveSpecifiedPostfixFromOneArticle_PostfixIsRemoved()
+        {
+            // Arrange
+            var worker = CreateWorker(new[] { Bikini }, TypTowaru.Towar, null, " damski");
+
+            // Act
+            worker.ZmianaNazw();
+
+            // Assert
+            Assert.AreEqual("Bikini - Strój kąpielowy", Bikini.Nazwa);
+        }
+
+        [Test]
+        public void ZmianaNazw_RemoveSpecifiedPostfixFromMultipleArticles_PostfixIsRemovedFromArticlesThatHaveSpecifiedPostfix()
+        {
+            // Arrange
+            var worker = CreateWorker(new[] { Bikini, Buty45 }, TypTowaru.Towar, null, " damski");
+
+            // Act
+            worker.ZmianaNazw();
+
+            // Assert
+            Assert.AreEqual("Bikini - Strój kąpielowy", Bikini.Nazwa);
+            Assert.AreEqual("Buty do nart Extreme 45", Buty45.Nazwa);
+        }
+
+        private ZmianaNazwTowarowWorker CreateWorker(Towar[] towary, TypTowaru typTowaru,
+            string dodajPostfix, string usunPostfix = null)
+        {
+            return new ZmianaNazwTowarowWorker(Context)
+            {
+                Towary = towary,
+                Params = new ZmianaNazwTowarowParams(Context)
+                {
+                    DodajPostfiks = dodajPostfix,
+                    UsunPostfiks = usunPostfix,
+                    TypTowaru = typTowaru
+                }
+            };
+        }
+
+        private Towar Bikini => Get<Towar>(new Guid("65336878-70cf-4e64-bd72-b742cd26a657"));
+        private Towar Buty45 => Get<Towar>(new Guid("61e50151-9f26-4de3-85dc-5d0983f56956"));
+        private Towar Montaz => Get<Towar>(new Guid("0f8a8597-e2d1-40a6-a8e5-cc1045228660"));
+    }
+}
