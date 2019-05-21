d23a24ae68f52f7b9b2e48069607318a8189b27b
Tomasz Ściepłek <tomasz.scieplek@enova.pl>
2019-05-21 15:26:01 +0200

    Podwaliny testów w dodatku EnovaDB

---
+++ tests/EnovaDB.Tests/EnsureTestedLibraryIsLoaded.cs
@@ -0,0 +1,15 @@
+﻿using EnovaDB.Punktacja;
+using NUnit.Framework;
+
+namespace EnovaDB.Integration.Tests
+{
+    [SetUpFixture]
+    public class Config
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _ = new PunktyDokumentuWorker();
+        }
+    }
+}
