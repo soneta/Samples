3fd05f1bef2b7e50a5f3be8984ef4dba2d83205c
Tomasz Ściepłek <tomasz.scieplek@enova.pl>
2019-05-22 12:46:34 +0200

    Rozbicie metod testujących kontrahentów lojalnych/nielojalnych

--- tests/Samples.Tests/KalkulatorRabatuTests.cs
+++ tests/Samples.Tests/KalkulatorRabatuTests.cs
@@ -5,24 +5,27 @@ namespace Samples.Tests
 {
     class KalkulatorRabatuTests
     {
-        [TestCase(10, false, ExpectedResult = 0)]
-        [TestCase(99, false, ExpectedResult = 0)]
-        [TestCase(100, false, ExpectedResult = 0.2)]
-        [TestCase(499, false, ExpectedResult = 0.2)]
-        [TestCase(500, false, ExpectedResult = 0.5)]
-        [TestCase(999, false, ExpectedResult = 0.5)]
-        [TestCase(1000, false, ExpectedResult = 1)]
-        // Lojalny kontrahent
-        [TestCase(10, true, ExpectedResult = 0)]
-        [TestCase(49, true, ExpectedResult = 0)]
-        [TestCase(50, true, ExpectedResult = 0.2)]
-        [TestCase(249, true, ExpectedResult = 0.2)]
-        [TestCase(250, true, ExpectedResult = 0.5)]
-        [TestCase(499, true, ExpectedResult = 0.5)]
-        [TestCase(500, true, ExpectedResult = 1)]
-        [TestCase(999, true, ExpectedResult = 1)]
-        [TestCase(1000, true, ExpectedResult = 1)]
-        public decimal Policz(decimal wartosc, bool lojalny) => KalkulatorRabatu.PoliczRabat( () => wartosc, () => lojalny);
+        [TestCase(10, ExpectedResult = 0)]
+        [TestCase(99, ExpectedResult = 0)]
+        [TestCase(100, ExpectedResult = 0.2)]
+        [TestCase(499, ExpectedResult = 0.2)]
+        [TestCase(500, ExpectedResult = 0.5)]
+        [TestCase(999, ExpectedResult = 0.5)]
+        [TestCase(1000, ExpectedResult = 1)]
+        public decimal Policz_KontrahentNielojalny(decimal wartosc) => 
+            KalkulatorRabatu.PoliczRabat( () => wartosc, () => false);
+
+        [TestCase(10, ExpectedResult = 0)]
+        [TestCase(49, ExpectedResult = 0)]
+        [TestCase(50, ExpectedResult = 0.2)]
+        [TestCase(249, ExpectedResult = 0.2)]
+        [TestCase(250, ExpectedResult = 0.5)]
+        [TestCase(499, ExpectedResult = 0.5)]
+        [TestCase(500, ExpectedResult = 1)]
+        [TestCase(999, ExpectedResult = 1)]
+        [TestCase(1000, ExpectedResult = 1)]
+        public decimal Policz_KontrahentLojalny(decimal wartosc) => 
+            KalkulatorRabatu.PoliczRabat(() => wartosc, () => true);
 
         [Test]
         public void LojalnyKontrahent_5DokumentowZOstatniegoPolrocza_True()
