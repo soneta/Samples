548a5adc67813fca949764889161d2fdb5a77961
Artur Żurman <artur.zurman@soneta.pl>
2020-02-13 09:31:31 +0100

    Przeniesienie logiki z przykładu 1 ( Soneta.Examples/Example1 )

---
+++ src/Samples/Samples/Config/Extender/Menu.cs
@@ -0,0 +1,20 @@
+﻿
+using Soneta.Business.Licence;
+using Soneta.Business.UI;
+using Samples.Lists.Extender;
+
+
+[assembly: FolderView("Samples",
+    Priority = 10,
+    Description = "Przykłady implementacji enova365",
+    BrickColor = FolderViewAttribute.BlueBrick,
+    Icon = "Samples.Utils.examples.ico;Samples",
+    Contexts = new object[] { LicencjeModułu.All }
+)]
+
+[assembly: FolderView("Samples/Towary własne",
+    Priority = 11,
+    Description = "Towary ulubione osoby kontaktowej",
+    TableName = "Towary",
+    ViewType = typeof(TowaryUlubioneKontaktuViewInfo)
+)]
---
+++ src/Samples/Samples/Lists/Extender/TowaryUlubioneKontaktuViewInfo.cs
@@ -0,0 +1,121 @@
+﻿using Soneta.Business;
+using Soneta.CRM;
+using Samples.Config.Extender;
+using Soneta.Towary;
+
+namespace Samples.Lists.Extender
+{
+    /// <summary>
+    /// Lista oparta na przykładzie pochodzącym z repozytorium Soneta.Examples - https://github.com/soneta/Examples (Example 1)
+    /// 
+    /// </summary>
+    public class TowaryUlubioneKontaktuViewInfo : ViewInfo
+    {
+
+        public TowaryUlubioneKontaktuViewInfo()
+        {
+            // View wiążemy z odpowiednią definicją viewform.xml poprzez property ResourceName
+            ResourceName = "TowaryUlubioneKontaktu";
+
+            // Inicjowanie contextu
+            InitContext += (sender, args) => { args.Context.TryAdd(() => new WParams(args.Context)); };
+
+            // Tworzenie view zawierającego konkretne dane
+            CreateView += TowaryWlasneViewInfo_CreateView;
+        }
+
+        void TowaryWlasneViewInfo_CreateView(object sender, CreateViewEventArgs args)
+        {
+            WParams parameters;
+            if (!args.Context.Get(out parameters))
+                return;
+            args.View = ViewCreate(parameters);
+            args.View.AllowNew = false;
+        }
+
+        protected View ViewCreate(WParams pars)
+        {
+
+            var condUlubione = RowCondition.Empty;
+            var rc = RowCondition.Empty;
+            var tm = TowaryModule.GetInstance(pars.Context.Session);
+            var view = tm.Towary.CreateView();
+
+            if (pars.KontaktOsoba == null)
+                condUlubione &= new FieldCondition.Null("Zapis", true);
+            else
+                condUlubione &= new FieldCondition.Equal("Zapis", pars.KontaktOsoba);
+
+            rc &= new FieldCondition.Equal("Blokada", false);
+            rc &= new FieldCondition.Exists("TowaryUlubione", "Towar", condUlubione);
+            view.Condition &= rc;
+
+            return view;
+        }
+
+        #region Widoczność zakładki
+
+        /// <summary>
+        /// Metoda pozwalająca na sterowanie widocznościa zakładki.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns>
+        ///     true - widoczność zakładki, 
+        ///     false - zakładka niewidoczna
+        /// </returns>
+        public static bool IsVisible(Context context)
+        {
+            bool result;
+            using (var session = context.Login.CreateSession(true, true))
+            {
+                result = SamplesConfigExtender.IsAktywneZakladkaSamples(session);
+            }
+            return result;
+        }
+
+        #endregion Widoczność zakładki
+    }
+
+    public class WParams : ContextBase
+    {
+        private const string Key = "Soneta.Examples.TowaryUlubione";
+
+        public WParams(Context context) : base(context)
+        {
+            Load();
+        }
+
+        public KontaktOsoba KontaktOsoba
+        {
+            get
+            {
+                if (Context.Contains(typeof(KontaktOsoba)))
+                    return (KontaktOsoba)Context[typeof(KontaktOsoba)];
+                return null;
+            }
+            set
+            {
+                Context[typeof(KontaktOsoba)] = value;
+                Save();
+            }
+        }
+
+        /// <summary>
+        /// Ładowanie parametrów z kontekstu login'a
+        /// </summary>
+        protected void Load()
+        {
+            var property = Context.LoadProperty(this, "KontaktOsoba", Key);
+            SetContext(typeof(KontaktOsoba), property);
+        }
+
+        /// <summary>
+        /// Zapisywanie parametrów w kontekście login'a
+        /// </summary>
+        protected void Save()
+        {
+            Context.SaveProperty(this, "KontaktOsoba", Key);
+        }
+    }
+
+}
