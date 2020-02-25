07ae460cbac93241b2d37878134e6f47c030ad81
Artur Żurman <artur.zurman@soneta.pl>
2020-02-25 13:29:16 +0100

    Example10

---
+++ src/Example10/Example10/Extender/CennikSerwis.cs
@@ -0,0 +1,15 @@
+﻿using Soneta.Business;
+using Samples.Example10.Extender;
+
+[assembly: Service(typeof(ICennikSerwis), typeof(CennikSerwis), Published = true)]
+namespace Samples.Example10.Extender {
+
+    public partial class CennikSerwis : ICennikSerwis {
+
+        private readonly Session _session;
+
+        public CennikSerwis(Session session) {
+            _session = session;
+        }
+    }
+}
---
+++ src/Example10/Example10/Extender/ICennikSerwis.cs
@@ -0,0 +1,6 @@
+﻿namespace Samples.Example10.Extender {
+    interface ICennikSerwis {
+        string ImportujCennik();
+        string EksportujCennik(string tsvContent);
+    }
+}
