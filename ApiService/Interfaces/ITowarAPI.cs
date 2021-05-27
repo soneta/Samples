c39610c00c3235bbef840ad17d78b6551cd45644
Michał Zembik <michal.zembik@soneta.pl>
2021-05-27 09:00:50 +0200

    Rejestracja serwisu

--- ApiService/Interfaces/ITowarAPI.cs
+++ ApiService/Interfaces/ITowarAPI.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ApiService
+namespace WebApiService.Interfaces
 {
-    public class Class1
+    public interface ITowarAPI
     {
+        string NazwaTowaru();
     }
 }
---
+++ ApiService/TowarWebApi.cs
@@ -0,0 +1,21 @@
+﻿using WebApiService;
+using Soneta.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiService.Interfaces;
+
+[assembly: Service(typeof(ITowarAPI), typeof(TowarWebApi), Published = true)]
+
+namespace WebApiService
+{
+    public class TowarWebApi : ITowarAPI
+    {
+        public string NazwaTowaru()
+        {
+            return "Pewna nazwa towaru";
+        }
+    }
+}
