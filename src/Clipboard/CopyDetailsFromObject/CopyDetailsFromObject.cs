951fffd4ad12ba47f708a191ce82db22cb28242d
Anna Lewicka <anna.lewicka@soneta.pl>
2021-06-10 14:48:32 +0200

    Nowości w enova365 - NamedStream, Clipboard

---
+++ src/Clipboard/CopyDetailsFromObject/CopyDetailsFromObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Soneta.Business;
+using Soneta.Business.UI;
+using Soneta.Commands;
+
+[assembly: Worker(typeof(CopyDetailsFromObject.CopyDetailsFromObject), typeof(GuidedRow))]
+
+namespace CopyDetailsFromObject
+{
+    internal class CopyDetailsFromObject
+    {
+
+        [Context] public GuidedRow[] GuidedRows { get; set; }
+
+        [Action("Kopiuj dane",
+            Target = ActionTarget.ToolbarWithText | ActionTarget.Menu | ActionTarget.LocalMenu | ActionTarget.Divider,
+            Mode = ActionMode.SingleSession,
+            Priority = 1,
+            CommandShortcut = CommandShortcut.Control | CommandShortcut.C)]
+        public object CopyData()
+        {
+            var text = "";
+            foreach (var guidedRow in GuidedRows)
+            foreach (var property in guidedRow.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly))
+                if (property.CanRead)
+                    text += property.Name + ": " + property.GetValue(guidedRow, null) + "\n";
+            return new ClipboardStream(text);
+        }
+    }
+
+}
---
+++ src/NamedStream/SaveRaportOnDisc/SaveDiploma.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using SaveDiplomaOnDisc;
+using Soneta.Business;
+using Soneta.Kadry;
+
+[assembly: Worker(typeof(SaveDiploma), typeof(Pracownicy))]
+namespace SaveDiplomaOnDisc
+{
+    internal class SaveDiploma
+    {
+        [Context] public Pracownik[] Pracownicy { get; set; }
+
+        [Action("Utwórz dane do drukarni",
+            Target = ActionTarget.Menu,
+            Mode = ActionMode.SingleSession,
+            Priority = 0)]
+        public NamedStream SaveData()
+        {
+            var memoryStream = new MemoryStream();
+            var document = new Document(PageSize.A4, 10, 10, 10, 10);
+            PdfWriter.GetInstance(document, memoryStream);
+            document.Open();
+            foreach (var pracownik in Pracownicy)
+                CreatePage(document, pracownik);
+            document.Close();
+            var bytes = memoryStream.ToArray();
+            memoryStream.Close();
+            return new NamedStream("dyplomy.pdf", bytes);
+        }
+
+        private static void CreatePage(Document document, Pracownik pracownik)
+        {
+            document.NewPage();
+            document.AddTitle("Dyplom");
+            var headerParagraph = new Paragraph("DYPLOM \n\n") {Alignment = Element.ALIGN_CENTER};
+            document.Add(headerParagraph);
+            var para = new Paragraph("Nagroda dla pracownika: " + pracownik.Nazwa + "\n za szczególne osiągnięcia w pracy") { Alignment = Element.ALIGN_CENTER }; ;
+            document.Add(para);
+        }
+    }
+}
